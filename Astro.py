2061221b49c88932886570539c1823c47729b97b
paragonmac <paragonmac@gmail.com>
2025-03-12 13:47:09 -0400

    Updated Notebook with new graphs and visuals

--- Astro.py
+++ Astro.py
@@ -1,7 +1,7 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
-hdul = fits.open(r'F:\Astroimages\M31\M31 6-18-23\Light_M31_300.0s_Bin1_2600MC_20230618-035346_0001.fit')
+hdul = fits.open(r'Data\M31\M31 6-18-23\Light_M31_300.0s_Bin1_2600MC_20230618-035346_0001.fit')
 hdul.info()
 
 for i, hdu in enumerate(hdul):
